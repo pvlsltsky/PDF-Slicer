bbb6841435d1db62587cf5afc28e461b48ef9ee7
Pavel Slutsky <pvlsltsky@gmail.com>
2024-01-23 23:58:46 +0200

    Resolved an issue with icon on a window

--- src/main_app.py
+++ src/main_app.py
@@ -1,11 +1,19 @@
 import sys
 import os.path
 from PySide6.QtCore import Slot
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QPixmap
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
 from splitted_area import MainWindowSplitted
 
 user_dir = os.path.expanduser("~")
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 class PDFMergerMainWindow(QMainWindow):
     def __init__(self):
@@ -16,6 +24,7 @@ class PDFMergerMainWindow(QMainWindow):
         self.setCentralWidget(splitted_area)
         self.setWindowTitle('PDF Merger')
         self.setStatusText("Ready")
+        self.setWindowIcon(QIcon(QPixmap(resource_path("logo200.ico"))))
     
     @Slot(str)
     def setStatusText(self, msg : str):
